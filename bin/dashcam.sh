88cf9965e446d1fc11b5a7c7d0838b64df163d02
Jan Majek (Yanchix Ltd) <jan.majek@yanchix.com>
2021-05-27 20:27:51 +0100

    Add path variables

--- bin/dashcam.sh
+++ bin/dashcam.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+
+SOURCE_VID="/Volumes/DASHCAM/DCIM/100MEDIA"
+TARGET_VID="/Volumes/My Passport for Mac/Dashcam/Video"
+
+echo "Source Video: $SOURCE_VID"
+echo "Target Video: $TARGET_VID"
+echo "========================================"
+
+###################################################################################################
+### Read and process all regular files from the SOURCE_VID using `for`
+### HOWTO: https://www.cyberciti.biz/tips/handling-filenames-with-spaces-in-bash.html
+###################################################################################################
+
 IFS_ORIG=$IFS
 IFS=$(echo -en "\n\b")
 echo "IFS_ORIG: '$IFS_ORIG'"
 echo "IFS: '$IFS'"
 echo "====="
-for file in /Volumes/DASHCAM/DCIM/100MEDIA/*; do
+for file in "$SOURCE_VID"/*; do
   if [[ ! -f "$file" ]]; then continue; fi
 
   mDateYMD=$(date -r "$file" +"%Y-%m-%d")
@@ -14,7 +27,7 @@ for file in /Volumes/DASHCAM/DCIM/100MEDIA/*; do
   FILENAME=$(basename "$file")
 
   echo "$file"
-  echo "/Volumes/My Passport for Mac/Dashcam/Video/$mDateYM/$mDateYMD/$mTime $FILENAME"
+  echo "$TARGET_VID/$mDateYM/$mDateYMD/$mTime $FILENAME"
   echo  "-----"
 done
 IFS=$IFS_ORIG
