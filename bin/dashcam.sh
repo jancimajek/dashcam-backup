57c11a0a1e9b7a8bdc4685a66af1ed33fb32d68a
Jan Majek (Yanchix Ltd) <jan.majek@yanchix.com>
2021-05-31 11:08:05 +0100

    Improve the file loop

--- bin/dashcam.sh
+++ bin/dashcam.sh
@@ -16,30 +16,76 @@ echo "========================================"
 
 
 ###################################################################################################
+### Read and process all regular files from the $sourceDir using `find` and arrays
+### HOWTO: https://www.cyberciti.biz/tips/handling-filenames-with-spaces-in-bash.html
+###################################################################################################
+
+# Read all files into array
+OLDIFS=$IFS
+IFS=$'\n' 
+fileArray=($(find "$SOURCE_VID" -type f -not -name ".*" | sort))
+IFS=$OLDIFS
+
+# Process files in the array
+for (( i=0; i<${#fileArray[@]}; i++ )); do
+  file=${fileArray[$i]}
+
+  # Generate date/timestamps based on file modified time
+  mDateYMD=$(date -r "$file" +"%Y-%m-%d")
+  mDateYM=$(date -r "$file" +"%Y-%m")
+  mTime=$(date -r "$file" +"%Y-%m-%d-%H-%M-%S")
+
+  # Generate target dir & file name
+  targetDir="$TARGET_VID/$mDateYM/$mDateYMD"
+  targetFile="$targetDir/$mTime-$(basename "$file")"
+
+  # Create target dir with all missing parent dirs if it doesn't already exist
+  if [[ ! -e "$targetDir" ]]; then
+    mkdir -pv "$targetDir"
+  fi
+
+  # Copy and rename file into the target dir without overwriting
+  # file --> TARGET_VID/YYYY-MM/YYYY-MM-DD/YYYY-MM-DD-HH-MM-SS-FILENAME.EXT
+  set +e
+  cp -nvp "${file}" "$targetFile"
+  retVal=$?
+  set -e
+
+  echo "Source:  $file"
+  echo "Target:  $targetFile"
+  echo "RetVal:  $retVal"
+  echo "Copied:  $copied"
+  echo "Skipped: $skipped"
+  echo  "-----"
+done
+
+
+###################################################################################################
+### ALTERNATIVE:
 ### Read and process all regular files from the SOURCE_VID using `for`
 ### HOWTO: https://www.cyberciti.biz/tips/handling-filenames-with-spaces-in-bash.html
 ###################################################################################################
 
-IFS_ORIG=$IFS
-IFS=$(echo -en "\n\b")
-echo "IFS_ORIG: '$IFS_ORIG'"
-echo "IFS: '$IFS'"
-echo "====="
-for file in "$SOURCE_VID"/*; do
-  if [[ ! -f "$file" ]]; then continue; fi
+# IFS_ORIG=$IFS
+# IFS=$(echo -en "\n\b")
+# echo "IFS_ORIG: '$IFS_ORIG'"
+# echo "IFS: '$IFS'"
+# echo "====="
+# for file in "$SOURCE_VID"/*; do
+#   if [[ ! -f "$file" ]]; then continue; fi
 
-  mDateYMD=$(date -r "$file" +"%Y-%m-%d")
-  mDateYM=$(date -r "$file" +"%Y-%m")
-  mTime=$(date -r "$file" +"%Y-%m-%dT%H.%m.%S")
-  FILENAME=$(basename "$file")
+#   mDateYMD=$(date -r "$file" +"%Y-%m-%d")
+#   mDateYM=$(date -r "$file" +"%Y-%m")
+#   mTime=$(date -r "$file" +"%Y-%m-%dT%H.%m.%S")
+#   FILENAME=$(basename "$file")
 
-  echo "$file"
-  echo "$TARGET_VID/$mDateYM/$mDateYMD/$mTime $FILENAME"
-  echo  "-----"
-done
-IFS=$IFS_ORIG
+#   echo "$file"
+#   echo "$TARGET_VID/$mDateYM/$mDateYMD/$mTime $FILENAME"
+#   echo  "-----"
+# done
+# IFS=$IFS_ORIG
 
-echo "IFS_ORIG: '$IFS_ORIG'"
-echo "IFS: '$IFS'"
-echo "====="
+# echo "IFS_ORIG: '$IFS_ORIG'"
+# echo "IFS: '$IFS'"
+# echo "====="
 
