3fa249f3708794bd94b33497636d38cba092abd8
Jan Majek (Yanchix Ltd) <jan.majek@yanchix.com>
2021-06-03 10:24:40 +0100

    Copy GPS logs as well

--- bin/dashcam.sh
+++ bin/dashcam.sh
@@ -5,77 +5,91 @@ set -euo pipefail
 IFS=$'\n\t'
 
 
-SOURCE_VID="/Volumes/DASHCAM/DCIM/100MEDIA"
-TARGET_VID="/Volumes/My Passport for Mac/Dashcam/Video"
+SOURCE="/Volumes/DASHCAM"
+SOURCE_GPS="${SOURCE}/gps"
+SOURCE_VID="${SOURCE}/DCIM/100MEDIA"
+
+TARGET="/Volumes/My Passport for Mac"
+TARGET_GPS="${TARGET}/Dashcam/GPS"
+TARGET_VID="${TARGET}/Dashcam/Video"
 
 
-echo "Source Video: $SOURCE_VID"
-echo "Target Video: $TARGET_VID"
-echo "========================================"
+function copyFiles() {
+  local source="$1"
+  local target="$2"
+
+  echo "Source: $source"
+  echo "Target: $target"
+  echo "----------------------------------------"
 
 
+  ###################################################################################################
+  ### Read and process all regular files from the $sourceDir using `find` and arrays
+  ### HOWTO: https://www.cyberciti.biz/tips/handling-filenames-with-spaces-in-bash.html
+  ###################################################################################################
 
-###################################################################################################
-### Read and process all regular files from the $sourceDir using `find` and arrays
-### HOWTO: https://www.cyberciti.biz/tips/handling-filenames-with-spaces-in-bash.html
-###################################################################################################
+  # Initiate counters
+  copied=0
+  skipped=0
 
-# Initiate counters
-copied=0
-skipped=0
+  # Read all files into array
+  OLDIFS=$IFS
+  IFS=$'\n' 
+  fileArray=($(find "$source" -type f -not -name ".*" | sort))
+  IFS=$OLDIFS
 
-# Read all files into array
-OLDIFS=$IFS
-IFS=$'\n' 
-fileArray=($(find "$SOURCE_VID" -type f -not -name ".*" | sort))
-IFS=$OLDIFS
+  # Process files in the array
+  for (( i=0; i<${#fileArray[@]}; i++ )); do
+    file=${fileArray[$i]}
 
-# Process files in the array
-for (( i=0; i<${#fileArray[@]}; i++ )); do
-  file=${fileArray[$i]}
+    # Generate date/timestamps based on file modified time
+    mDateYMD=$(date -r "$file" +"%Y-%m-%d")
+    mDateYM=$(date -r "$file" +"%Y-%m")
+    mTime=$(date -r "$file" +"%Y-%m-%d-%H-%M-%S")
 
-  # Generate date/timestamps based on file modified time
-  mDateYMD=$(date -r "$file" +"%Y-%m-%d")
-  mDateYM=$(date -r "$file" +"%Y-%m")
-  mTime=$(date -r "$file" +"%Y-%m-%d-%H-%M-%S")
+    # Generate target dir & file name
+    targetDir="$target/$mDateYM/$mDateYMD"
+    targetFile="$targetDir/$mTime-$(basename "$file")"
 
-  # Generate target dir & file name
-  targetDir="$TARGET_VID/$mDateYM/$mDateYMD"
-  targetFile="$targetDir/$mTime-$(basename "$file")"
+    # Create target dir with all missing parent dirs if it doesn't already exist
+    if [[ ! -e "$targetDir" ]]; then
+      mkdir -pv "$targetDir"
+    fi
 
-  # Create target dir with all missing parent dirs if it doesn't already exist
-  if [[ ! -e "$targetDir" ]]; then
-    mkdir -pv "$targetDir"
-  fi
+    # Copy and rename file into the target dir without overwriting
+    # file --> TARGET_VID/YYYY-MM/YYYY-MM-DD/YYYY-MM-DD-HH-MM-SS-FILENAME.EXT
+    set +e
+    cp -nvp "${file}" "$targetFile"
+    retVal=$?
+    set -e
 
-  # Copy and rename file into the target dir without overwriting
-  # file --> TARGET_VID/YYYY-MM/YYYY-MM-DD/YYYY-MM-DD-HH-MM-SS-FILENAME.EXT
-  set +e
-  cp -nvp "${file}" "$targetFile"
-  retVal=$?
-  set -e
+    # Increment appropriate counters and handle errors
+    if [[ $retVal -eq 0 ]]; then 
+      ((copied++))
+    else 
+      ((skipped++))
+    fi
 
-  # Increment appropriate counters and handle errors
-  if [[ $retVal -eq 0 ]]; then 
-    ((copied++))
-  else 
-    ((skipped++))
-  fi
+    # echo "Source:  $file"
+    # echo "Target:  $targetFile"
+    # echo "RetVal:  $retVal"
+    # echo "Copied:  $copied"
+    # echo "Skipped: $skipped"
+    # echo  "-----"
+  done
 
-  # echo "Source:  $file"
-  # echo "Target:  $targetFile"
-  # echo "RetVal:  $retVal"
-  # echo "Copied:  $copied"
-  # echo "Skipped: $skipped"
-  # echo  "-----"
-done
+  # Print out stats
+  echo "----------------------------------------"
+  echo "Copied:  $copied"
+  echo "Skipped: $skipped"
+  echo "TOTAL:   $(($copied+$skipped))"
+  echo "========================================"
 
-# Print out stats
-echo "----------------------------------------"
-echo "Copied:  $copied"
-echo "Skipped: $skipped"
-echo "TOTAL:   $(($copied+$skipped))"
-echo "========================================"
+}
+
+
+copyFiles "$SOURCE_GPS" "$TARGET_GPS"
+copyFiles "$SOURCE_VID" "$TARGET_VID"
 
 
 ###################################################################################################
